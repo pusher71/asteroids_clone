ce30973f6220be94e6361fecc528849b20484f76
Bogdan <bogdanbondarenko19@yandex.ru>
2021-06-28 06:29:39 +0300

    First commit

---
+++ Assets/Scripts/PolygonalSpriteView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolygonalSpriteView : MonoBehaviour
+{
+    [SerializeField] protected GameObject polygonView; //полигональное представление
+    [SerializeField] protected GameObject spriteView; //спрайтовое представление
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //отобразить одно из представлений
+        //polygonView.SetActive(ControllerScript.instance.polygonView);
+        //spriteView.SetActive(!ControllerScript.instance.polygonView);
+    }
+}
